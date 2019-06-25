04ead1030dd2b2643742f36a9a5f1f0e175fcbff
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-06-25 14:52:55 -0600

    Controllers finished

--- MyPokedex/Model Control/PokemonController.swift
+++ MyPokedex/Model Control/PokemonController.swift
@@ -7,9 +7,68 @@
 //
 
 import Foundation
+import UIKit
 
 class PokemonController {
     
+    //Singleton - allow access to the items on this class
+    static let sharedInstance = PokemonController()
     
+    //Create
+    //Method signature - begin to access the data from the url to create pokemon!
+    func fetchPokemonWith(searchTerm: String, completion: @escaping((TopLevelDictionary?) -> Void)) {
+        
+        //1.Build the base URL (unwrap if needed)
+        let baseUrl = URL(string: "https://pokeapi.co/api/v2/")
+        
+        //2. construct the final url (ex: appending anything to the url)
+        //(add /pokemon, add /searchTerm)
+        let pokemonPathComponentURL = baseUrl?.appendingPathComponent("pokemon")
+        guard let finalURL = pokemonPathComponentURL?.appendingPathComponent(searchTerm) else { return }
+        
+        //3. create a url request to get data from
+        //        let request = URLRequest(url: url)
+        
+        //4. get the data from the url request
+        URLSession.shared.dataTask(with: finalURL) { (data, _, error) in
+            //handle the error
+            if let error = error {
+                print("There was an error \(error.localizedDescription)")
+            }
+            //check if there is data
+            if let data = data {
+                do {
+                    //decode the data into an object we can use
+                    //handle the data
+                    let pokemon = try JSONDecoder().decode(TopLevelDictionary.self, from: data)
+                    completion(pokemon)
+                } catch {
+                    print("Error fetching pokemon!")
+                    completion(nil); return
+                }
+            }
+        } .resume()
+    }
     
+    func fetchPokeImage(pokemon: TopLevelDictionary, completion: @escaping (UIImage?) -> Void) {
+        // Build URL
+        let imageURL = pokemon.spritesDictionary.image
+        URLSession.shared.dataTask(with: imageURL) { (data, _, error) in
+            if let error = error {
+                print("Error with image \(error.localizedDescription)")
+            }
+            if let data = data {
+                guard let pokeImage = UIImage(data: data) else { completion(nil); return }
+                completion(pokeImage)
+            }
+        } .resume()
+    }
 }
+
+//background thread
+//need to know when work in background is done - completion allows to run code when something is done
+
+//optionals because we arent sure we will get the data we want back from the api
+//return void so if we dont get that specific data, we choose to get nothing.
+
+//@escaping
--- MyPokedex/View Control/PokemonViewController.swift
+++ MyPokedex/View Control/PokemonViewController.swift
@@ -10,24 +10,52 @@ import UIKit
 
 class PokemonViewController: UIViewController {
 
-    
+    //Outlets
     @IBOutlet weak var pokeSearchBar: UISearchBar!
+    @IBOutlet weak var pokeImageVIew: UIImageView!
+    @IBOutlet weak var pokeNameLabel: UILabel!
+    @IBOutlet weak var pokeIDLabel: UILabel!
+    @IBOutlet weak var pokeAbilityLabel: UILabel!
     
+    //Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        //How we get info from the search bar
+        pokeSearchBar.delegate = self
+        let searchBar = UISearchBar()
+        searchBar.textField?.textColor = UIColor.white // or whichever color you want
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updatesView(pokeball: TopLevelDictionary) {
+        DispatchQueue.main.async {
+        self.pokeNameLabel.text = pokeball.name.uppercased()
+        self.pokeIDLabel.text = "\(pokeball.id)"
+        self.pokeAbilityLabel.text = pokeball.abilities[0].ability.name.uppercased()
+        }
     }
-    */
+}
 
+//Make class conform to searchbar protocol through a class extension
+extension PokemonViewController: UISearchBarDelegate {
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        //call data
+        PokemonController.sharedInstance.fetchPokemonWith(searchTerm: searchText) { (pokemon) in
+            guard let unwrappedPoke = pokemon else { return }
+            PokemonController.sharedInstance.fetchPokeImage(pokemon: unwrappedPoke, completion: { (image) in
+                DispatchQueue.main.async {
+                    self.pokeImageVIew.image = image
+                }
+            })
+            self.updatesView(pokeball: unwrappedPoke)
+        }
+    }
+}
+
+extension UISearchBar {
+    var textField: UITextField? {
+        return subviews.first?.subviews.compactMap { $0 as? UITextField }.first
+    }
 }
